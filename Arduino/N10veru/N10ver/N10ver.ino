5f9d514bb0f013c1077503d91718334d8263d7c0
mochi-insent <84953982+mochi-insent@users.noreply.github.com>
2022-12-14 21:55:06 +0900

    const追加

--- Arduino/N10veru/N10ver/N10ver.ino
+++ Arduino/N10veru/N10ver/N10ver.ino
@@ -1,4 +1,25 @@
 // N10veru
+
+const int LED_PIN  = 13;  //LED Pin
+const int M1_D_PIN  = 4;  //M1 Direction Pin
+const int M1_S_PIN  = 5;  //M1 Speed Pin
+const int M2_S_PIN  = 6;  //M2 Speed Pin
+const int M2_D_PIN  = 7;  //M2 Direction Pin
+const int S_CR_PIN  =11;  //Sensor CenterR Pin
+const int S_CL_PIN  =12;  //Sensor CenterL Pin
+const int S_OR_PIN  = 9;  //Sensor OuterR Pin
+const int S_OL_PIN = 10;  //Sensor OuterL Pin
+const int SPEED_H = 255;
+const int SPEED_M = 100;
+const int SPEED_Z   = 0;
+const int FWD       = HIGH; //LOW  if it go reverse 
+const int BWD       = LOW;  //HIGH if it goes reverse
+const int MOTOR_L   = 1;    //Motor selection parameter Left
+const int MOTOR_R   = 0;    //Motor selection parameter Right
+const int BLACK   = LOW;
+const int WHITE  = HIGH;
+
+
 void setup() {
  pinMode (LED_PIN, OUTPUT);
  pinMode (M1_D_PIN, OUTPUT);
