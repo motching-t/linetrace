4f164cde8246ce8c80cc550e73afd32030fbdd89
mochi-insent <84953982+mochi-insent@users.noreply.github.com>
2022-12-14 22:32:24 +0900

    コンパイルエラーとった

--- Arduino/N10veru/N10ver/N10ver.ino
+++ Arduino/N10veru/N10ver/N10ver.ino
@@ -19,6 +19,89 @@ const int MOTOR_R   = 0;    //Motor selection parameter Right
 const int BLACK   = LOW;
 const int WHITE  = HIGH;
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const int DIRECT = 1;  // CHECK !!!!!!!!!!!
+const int LOOP = 2;
+
+int initialB;
+int lineP;
+int modeTRACK;
+
+const int M_RH = 1;
+const int M_LH = 2;
+
+// ledBlinkN
+// const int LED_PIN = 13;
+// pinMode (LED_PIN, OUTPUT);
+
+int getPB () {
+ const int KEY_SENS_V[6] ={0,30,150,360,535,760};
+ const int KEY_KEEP = 100;
+ int keyVolt;
+ int i = 6;
+ int oldI = 0;
+ while (i != oldI) {
+ oldI = i;
+ delay (KEY_KEEP);
+ keyVolt = 0;
+ // keyVolt = analogRead(PB_PIN);
+ for (i = 1; i < 6; i++) {
+ if (keyVolt < KEY_SENS_V[i]) {
+ break;
+ }
+ }
+ }
+ if (i == 6) {
+ i = 0;
+ }
+ return i;
+}
+
+void ledBlinkN(int count, int onTime, int offTime) {
+ int i;
+ for (i = 0; i < count; i++) {
+ digitalWrite (LED_PIN, HIGH);
+ delay (onTime);
+ digitalWrite (LED_PIN, LOW);
+ delay (offTime);
+ }
+}
+
+void moveM (int motor, int dir, int speed) {
+ static int oldM_dir[2] = {0, 0};
+ static int oldM_speed[2] = {0, 0};
+ int M_S_PIN[2];
+ int M_D_PIN[2];
+ M_S_PIN[0] = M1_S_PIN;
+ M_S_PIN[1] = M2_S_PIN;
+ M_D_PIN[0] = M1_D_PIN;
+ M_D_PIN[1] = M2_D_PIN;
+ if ((oldM_dir[motor] != dir) &&
+ (oldM_speed[motor] != SPEED_Z) &&
+ (speed != SPEED_Z)) { // dir change then pause
+ analogWrite (M_S_PIN[motor], SPEED_Z);
+ delay (100);
+ }
+ digitalWrite (M_D_PIN[motor], dir);
+ analogWrite (M_S_PIN[motor], speed) ;
+ oldM_dir[motor] = dir;
+ oldM_speed[motor] = speed;
+}
+
+int wakeUp (int s) {
+ int keyIn;
+ while (true) { // loop
+ ledBlinkN (1, 250, 250); // Waiting LED flushing)
+ keyIn = getPB ( );
+ if (keyIn == 0) { // not pressed
+ continue;
+ }
+ if (keyIn <= s) { // Key is propper
+ break;
+ }
+ } // end -while
+ return keyIn;
+} 
 
 void setup() {
  pinMode (LED_PIN, OUTPUT);
@@ -34,6 +117,7 @@ void setup() {
  lineP = DIRECT;
  }
 }
+
 void loop() {
  int readSCR;
  int readSCL;
